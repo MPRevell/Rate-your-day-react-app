429dd1f39d8f2a9620047b2fbea04e711eb7e986
Matt <mprevell97@gmail.com>
2023-10-10 13:21:32 +0100

    Added shared button component with propertiesso that we can customise and reuse throughout the app

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Card from "./shared/Card";
+import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState();
@@ -20,7 +21,7 @@ function FeedbackForm() {
           placeholder="Leave a review"
           value={text}
         />
-        <button type="submit">Submit</button>
+        <Button type="submit">Submit</Button>
       </div>
     </Card>
   );
